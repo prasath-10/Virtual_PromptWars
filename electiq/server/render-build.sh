850884c72bd6cfc425e6dc31dc6721f7323497bf
Prasath <prasaths1007@gmail.com>
2026-04-29 14:15:10 +0530

    feat: add Render configuration and build script for full-stack deployment

--- electiq/server/render-build.sh
+++ electiq/server/render-build.sh
@@ -3,6 +3,4 @@
 set -o errexit
 
 npm install
-# No extra steps needed for puppeteer in latest versions of Render's Node runtime
-# but some users prefer to manually trigger browser download
-# npx puppeteer browsers install chrome
+npx puppeteer browsers install chrome
